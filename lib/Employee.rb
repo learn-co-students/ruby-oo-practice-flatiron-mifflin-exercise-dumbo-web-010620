6ca9e5d44d2c09f676de8a18566373eec1b096cb
github username <github email address>
2020-01-14 10:01:13 -0500

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -18,19 +18,20 @@ class Employee
     end
 
     def self.find_by_department(department)
-        self.all.find { |employee| employee.department == department }
+        self.all.find { |employee| employee.manager.department == department }
     end
 
     def tax_bracket
+        Employee.all.select do |employee| 
+            if employee.salary >= self.salary - 1000 && employee.salary <= self.salary + 1000
+                employee
+            end
+        end
     end
 
     def self.all
         @@all
     end
-
-# Employee.find_by_department
-#   takes a String argument that is the name of a department and returns the 
-    #first employee whose manager is working in that department
 # Employee#tax_bracket
 #   returns an Array of all the employees whose salaries are within $1000 
     #(± 1000) of the employee who invoked the method
--- run.rb
+++ run.rb
@@ -11,12 +11,12 @@ bill = Manager.new("Bill Gates", "Microsoft", 54)
 steve = Manager.new("Steve Jobs", "Apple", 52)
 
 #--------------------------Employee----------------------
-tyler1 = bill.hire_employee("Tyler", 80_000)
+tyler1 = bill.hire_employee("Tyler", 66_000)
 joey1 = bill.hire_employee("Joey", 65_000)
-leroy1 = steve.hire_employee("Leroy", 45_000)
-peter1 = steve.hire_employee("Peter", 75_000)
-jimmy1 = elon.hire_employee("Jimmy", 95_000)
-john1 = elon.hire_employee("John", 120_000)
+leroy1 = steve.hire_employee("Leroy", 62_000)
+peter1 = steve.hire_employee("Peter", 64_000)
+jimmy1 = elon.hire_employee("Jimmy", 68_000)
+john1 = elon.hire_employee("John", 67_000)
 
 
 
